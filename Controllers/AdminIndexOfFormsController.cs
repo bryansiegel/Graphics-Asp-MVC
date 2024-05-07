22e60dacd7d1328cd0fcfc0a74bcdb9b27da2bdc
Bryan Siegel <bryansiegel@gmail.com>
2024-05-07 11:05:21 -0700

    minor edits

--- Controllers/AdminIndexOfFormsController.cs
+++ Controllers/AdminIndexOfFormsController.cs
@@ -58,7 +58,7 @@ namespace Graphics_Asp_MVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FormName,active")] IndexOfForms indexOfForms)
+        public async Task<IActionResult> Create([Bind("Id,Title,active")] IndexOfForms indexOfForms)
         {
             if (ModelState.IsValid)
             {
